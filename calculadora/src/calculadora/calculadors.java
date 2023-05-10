08654a28a567ce32a6b4d6ca18dabc61db37758e
kikoview <francisco.vazquez@viewnext.com>
2023-05-10 09:50:50 +0200

    cambioss

--- calculadora/src/calculadora/calculadors.java
+++ calculadora/src/calculadora/calculadors.java
@@ -8,6 +8,7 @@ public class calculadors {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				while(true) {
+					System.out.print("\n-------------------------------");
 						System.out.print("\nElige un numero del 1 al 4");
 						System.out.print("\n");
 						System.out.print("1.Sumar: ");
